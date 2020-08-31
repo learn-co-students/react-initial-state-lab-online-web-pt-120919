10e20a923b17e82c485eec510fd48dee177aa9a2
Bouloute <annaick1995@gmail.com>
2020-08-31 09:03:57 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,36 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        /*
+        if (this.state.secondsLeft <= 0){
+            return (
+                <div>
+                    Boom!
+                </div>
+            )
+        }
+        else{
+            return (
+                <div>
+                    {this.state.secondsLeft} seconds left before I go boom!
+                </div>
+            )
+        }*/
+        
+        // didnt like the answer above so I peeked at the solution which is 100% prettier
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+        return (
+            <div>{message}</div>
+        )
+    }
+}
