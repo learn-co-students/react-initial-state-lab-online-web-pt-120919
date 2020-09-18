9eae2709bf88815c7cc957d526874703df63ed2f
Sehrish88 <sehrishbalouch4@gmail.com>
2020-09-18 15:43:26 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,4 +1,28 @@
 // your Bomb code here!
+
+function changeState(state, action) {
+ 
+    switch (action.type) {
+ 
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 }
+ 
+      default:
+        return state;
+    }
+  }
+ 
+  function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+ 
+function render(){
+  document.body.textContent = state.count
+}
+ 
+dispatch({ type: '@@INIT' })
+
 import React from 'react';
 
 class Bomb extends React.Component {
