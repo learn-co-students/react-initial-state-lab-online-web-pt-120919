5b3e05f795b8734eb753b1356eb151c444d3956c
Sehrish88 <sehrishbalouch4@gmail.com>
2020-09-18 15:44:27 -0400

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,4 +1,26 @@
 // your ImageSlider code here!
+function changeState(state, action) {
+ 
+    switch (action.type) {
+ 
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 }
+ 
+      default:
+        return state;
+    }
+  }
+ 
+  function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+ 
+function render(){
+  document.body.textContent = state.count
+}
+ 
+dispatch({ type: '@@INIT' })
 import React from 'react';
 
 class ImageSlider extends React.Component {
